4f6bd11e79c90f1083bdf7ccb25df8f09f3a8f14
Hưng Đinh <hung.dq2416937@sis.hust.edu.vn>
2026-06-29 11:40:22 +0700

    Rename Ex1 class to Ex21 and update logic

--- Ex1.java
+++ Ex1.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
-import java.lang.Math;
-public class Ex1 {
+public class Ex21 {
     public static void main(String[] args) {
-        float radius;
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("bán kính : ");
-        radius = scanner.nextFloat();
-        System.out.printf("Diện tích: %.2f",radius*Math.PI);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum+=i;
+        }
+        System.out.printf("Tổng của các số từ 1 đến %d là: %d",n,sum);
     }
 }
