ced6a77c8ce32be4c7a4757854c3426b47cd15d4
Hưng Đinh <hung.dq2416937@sis.hust.edu.vn>
2026-07-01 18:17:01 +0700

    Update Ex1.java

--- Ex1.java
+++ Ex1.java
@@ -1,12 +1,38 @@
 import java.util.Scanner;
-public class Ex21 {
+
+public class Ex41 {
+    public static void BubbleSort(int[] arr ){
+        int n = arr.length;
+        boolean flag = true;
+        for(int i=0;i<n-1;i++){
+            flag = false;
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]<arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    flag = true;
+                }
+            }
+            if(!flag){
+                break;
+            }
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập số lượng phần tử của mảng: ");
         int n = sc.nextInt();
-        int sum=0;
+        System.out.print("Nhập các phần tử của mảng:\n");
+        int[] mang = new int[n];
         for(int i=1;i<=n;i++){
-            sum+=i;
+            System.out.printf("Phần tử thứ %d: ",i);
+            mang[i-1] = sc.nextInt();
         }
-        System.out.printf("Tổng của các số từ 1 đến %d là: %d",n,sum);
+        BubbleSort(mang);
+        System.out.println("Mảng sau khi sắp xếp giảm dần");
+        for(int i=0;i<n;i++){
+            System.out.print(mang[i]+" ");
+        }
     }
 }
