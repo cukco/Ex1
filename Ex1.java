d9cd1c43a73b7c7306b658075639888db5547080
Hưng Đinh <hung.dq2416937@sis.hust.edu.vn>
2026-07-08 17:11:27 +0700

    Update Ex1.java

--- Ex1.java
+++ Ex1.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
-
-public class Ex51 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập địa chỉ email: ");
-        String email = scanner.nextLine().trim();
-        String emailRegex = "^[a-zA-Z0-9._]+@[a-zA-Z0-9.]+\\.[a-zA-Z]{2,6}$";
-        if (email.matches(emailRegex)) {
-            System.out.println("=>Email hợp lệ.");
-        } else {
-            System.out.println("Email không hợp lệ.");
-        }
-        scanner.close();
+class Student{
+    int id;
+    String name;
+    int age;
+    public Student(int id,String name,int age){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+    }
+}
+public class Ex71 {
+    public static void main(String[] args){
+        Student me=new Student(10,"Quang",20);
+        System.out.print("ID: "+me.id+",Name: "+me.name+",Age: "+me.age);
     }
 }
